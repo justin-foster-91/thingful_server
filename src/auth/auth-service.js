af0872f6b1c0794185f44697cf7ae08f22b8ae5b
Jim Smith <jsmith774@gmail.com>
2020-08-28 15:05:15 -0400

    additional jwt impl and tests

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -15,6 +15,11 @@ const AuthService = {
       algorithm: 'HS256',
     });
   },
+  verifyJwt(token) {
+    return jwt.verify(token, config.JWT_SECRET, {
+      algorithms: ['HS256'],
+    });
+  },
   parseBasicToken(token) {
     return Buffer.from(token, 'base64').toString().split(':');
   },
